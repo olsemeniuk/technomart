f17de52e1cea92d0106f1eb5cec9b8600fa26afd
Oleksandr Semeniuk <ol.semeniuk.mail@gmail.com>
2023-03-19 21:36:21 +0200

    focus on opened modal

--- js/main.js
+++ js/main.js
@@ -14,42 +14,55 @@ $(function () {
 
   // tabs
   const tabButton = document.querySelectorAll('.tabs__button'),
-        tabSection = document.querySelectorAll('.tabs__section');
+    tabSection = document.querySelectorAll('.tabs__section');
 
   tabButton.forEach(btn => {
     btn.addEventListener('click', () => {
-      for(let i = 0; i < tabButton.length; i++) {
+      for (let i = 0; i < tabButton.length; i++) {
         tabButton[i].classList.remove('tabs__button--active')
         tabButton[i].setAttribute('tabindex', '0')
       }
       btn.classList.add('tabs__button--active')
       btn.setAttribute('tabindex', '-1')
 
-      for(let i = 0; i < tabSection.length; i++) {
+      for (let i = 0; i < tabSection.length; i++) {
         tabSection[i].classList.remove('tabs__section--active')
-        if(btn.dataset.btn === tabSection[i].dataset.content) {
+        if (btn.dataset.btn === tabSection[i].dataset.content) {
           tabSection[i].classList.add('tabs__section--active')
         }
       }
     })
   })
 
-  // contact modal
-  const openModal = (modalName, openButton) => {
-    openButton.click(function(e) {
+  // modal
+  const openModal = (modalName, openButton, focusedElement) => {
+    openButton.click(function (e) {
       e.preventDefault();
       $('.modal').removeClass('modal--active');
       modalName.addClass('modal--active');
+
+      focusedElement.focus();
     });
   };
 
-  $('.modal-close, .close-cart').click(function (e) {
-    e.preventDefault();
-    $('.modal').removeClass('modal--active');
-  });
+  const closeModal = (closingElement) => {
+    closingElement.click(function (e) {
+      e.preventDefault();
+      $('.modal').removeClass('modal--active');
+    });
+  };
 
-  openModal($('.modal-contact'), $('.open-contact-modal'));
-  openModal($('.modal-map'), $('.contacts__map'));
-  openModal($('.modal-cart'), $('.open-cart-modal'));
+  openModal($('.modal-contact'), $('.open-contact-modal'), $('#contact-name-input'));
+  openModal($('.modal-map'), $('.contacts__map'), $('.modal-map iframe'));
+  openModal($('.modal-cart'), $('.open-cart-modal'), $('#go-to-cart'));
+
+  closeModal($('.modal-close'));
+  closeModal($('.close-cart'));
+
+  $(document).mouseup(function (e) {
+    if ($('.modal').has(e.target).length === 0) {
+      $('.modal').removeClass('modal--active');
+    };
+  });
 });
 
